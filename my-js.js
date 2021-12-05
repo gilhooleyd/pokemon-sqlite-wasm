e7ce8a66058c9ed657e996addeeafb374816d3e3
David <djgilhooley@gmail.com>
2021-12-05 13:04:50 -0800

    Initial Commit

---
+++ my-js.js
@@ -0,0 +1,55 @@
+async function my_code() {
+
+const SQL =  await initSqlJs({
+  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+  // You can omit locateFile completely when running in node
+  locateFile: file => `sql-wasm.wasm`
+});
+
+// Create a database
+const db = new SQL.Database();
+// NOTE: You can also use new SQL.Database(data) where
+// data is an Uint8Array representing an SQLite database file
+
+
+// Execute a single SQL string that contains multiple statements
+let sqlstr = "CREATE TABLE hello (a int, b char); \
+INSERT INTO hello VALUES (0, 'hello'); \
+INSERT INTO hello VALUES (1, 'world');";
+db.run(sqlstr); // Run the query without returning anything
+
+// Prepare an sql statement
+const stmt = db.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
+
+// Bind values to the parameters and fetch the results of the query
+const result = stmt.getAsObject({':aval' : 1, ':bval' : 'world'});
+console.log(result); // Will print {a:1, b:'world'}
+
+// Bind other values
+stmt.bind([0, 'hello']);
+while (stmt.step()) console.log(stmt.get()); // Will print [0, 'hello']
+// free the memory used by the statement
+stmt.free();
+// You can not use your statement anymore once it has been freed.
+// But not freeing your statements causes memory leaks. You don't want that.
+
+const res = db.exec("SELECT * FROM hello");
+/*
+[
+  {columns:['a','b'], values:[[0,'hello'],[1,'world']]}
+]
+*/
+
+// You can also use JavaScript functions inside your SQL code
+// Create the js function you need
+function add(a, b) {return a+b;}
+// Specifies the SQL function's name, the number of it's arguments, and the js function to use
+db.create_function("add_js", add);
+// Run a query in which the function is used
+db.run("INSERT INTO hello VALUES (add_js(7, 3), add_js('Hello ', 'world'));"); // Inserts 10 and 'Hello world'
+
+// Export the database to an Uint8Array containing the SQLite database file
+const binaryArray = db.export();
+  };
+
+my_code();
